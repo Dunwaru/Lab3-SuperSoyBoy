5fb81738a33658a38abdd58e596b252e48de74cf
E Kennedy <ekennedy103@gmail.com>
2022-12-02 10:15:45 -0500

    Chapter 13 - Added Player Anims to Player

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -48,6 +48,9 @@ public class SoyBoyController : MonoBehaviour
         // 1
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
+
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
+
         // 2
         if (input.x > 0f)
         {
@@ -61,10 +64,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
 
@@ -73,6 +78,7 @@ public class SoyBoyController : MonoBehaviour
             if (input.y > 0f)
             {
                 isJumping = true;
+                animator.SetBool("IsOnWall", false);
             }
         }
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -120,8 +126,19 @@ public class SoyBoyController : MonoBehaviour
 
         if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-            * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed
+            * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
         }
 
         if (isJumping && jumpDuration < jumpDurationThreshold)
